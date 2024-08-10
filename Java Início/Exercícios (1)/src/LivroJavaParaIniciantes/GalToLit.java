55b596f8f40be5687c766272a5bba97fb644ddf1
Francielle Limonge <152073275+naagaii@users.noreply.github.com>
2024-08-10 15:32:13 -0300

    exercicios livro java para iniciantes

---
+++ Java Início/Exercícios (1)/src/LivroJavaParaIniciantes/GalToLit.java
@@ -0,0 +1,11 @@
+package LivroJavaParaIniciantes;
+public class GalToLit {
+    public static void main(String[] args) throws Exception {
+        double gallons;
+        double liters;
+        gallons = 10;
+        liters = gallons * 3.7854;
+        System.out.println(gallons + " gallons is " + liters + " liters");
+
+    }
+}
---
+++ Java Início/Exercícios (1)/src/LivroJavaParaIniciantes/Inches.java
@@ -0,0 +1,12 @@
+package LivroJavaParaIniciantes;
+
+public class Inches {
+    public static void main(String[] args) {
+        long c1, im;
+        im = 5280 * 12; 
+        c1 = im * im * im;
+
+        System.out.println("There are " + c1 + " cubic inches in cubic mile. ");
+
+    }
+}
